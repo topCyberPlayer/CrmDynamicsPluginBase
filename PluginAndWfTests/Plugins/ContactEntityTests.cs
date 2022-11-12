4337dfb86e68be8c682def96b775bb25b4e6b9dd
Anton Mishin <ton4ik07@yandex.ru>
2022-11-12 14:49:08 +0300

    Added UnitTest

---
+++ PluginAndWfTests/Plugins/ContactEntityTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PluginAndWf.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginAndWf.Plugins.Tests
+{
+    [TestClass()]
+    public class ContactEntityTests
+    {
+        [TestMethod()]
+        public void ContactEntityTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
